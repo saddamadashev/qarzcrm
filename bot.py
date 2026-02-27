29e5f01d3e7a310cffffa59e85bd0edef264bd9f
saddamadashev <a.saddam77@icloud.com>
2026-02-27 13:50:21 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,19 +9,13 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-async def main():
-    print("Bot starting...")
-    
-    await init_db()
-    register_handlers(dp)
-    
-    print("Bot started successfully")
-    await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    asyncio.run(main())
-    async def main():
+async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await init_db()
     register_handlers(dp)
     await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
