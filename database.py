e7ad7beb72bc97612d750f990917e94da808ada4
saddamadashev <a.saddam77@icloud.com>
2026-02-28 18:12:43 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,33 +1,33 @@
 import aiosqlite
-from datetime import datetime
+
+DB_NAME = "finance.db"
 
 async def init_db():
-    async with aiosqlite.connect("qarz_daftar.db") as db:
-        # Mijozlar jadvali
-        await db.execute("""CREATE TABLE IF NOT EXISTS customers (
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER PRIMARY KEY,
+            joined_at TEXT
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS customers (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             owner_id INTEGER,
             name TEXT,
-            balance REAL DEFAULT 0,
-            last_update TEXT
-        )""")
-        # Tranzaksiyalar (Tarix va Chek uchun)
-        await db.execute("""CREATE TABLE IF NOT EXISTS transactions (
+            created_at TEXT
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS transactions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             cust_id INTEGER,
             amount REAL,
             type TEXT,
-            date TEXT
-        )""")
-        await db.commit()
+            created_at TEXT
+        )
+        """)
 
-async def add_transaction(cust_id, amount, t_type):
-    async with aiosqlite.connect("qarz_daftar.db") as db:
-        now = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
-        await db.execute("INSERT INTO transactions (cust_id, amount, type, date) VALUES (?,?,?,?)",
-                         (cust_id, amount, t_type, now))
-        if t_type == "PLUS":
-            await db.execute("UPDATE customers SET balance = balance + ?, last_update = ? WHERE id = ?", (amount, now, cust_id))
-        else:
-            await db.execute("UPDATE customers SET balance = balance - ?, last_update = ? WHERE id = ?", (amount, now, cust_id))
         await db.commit()
