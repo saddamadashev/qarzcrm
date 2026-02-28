c272782507b8daeab4cd427dd277c045c3d34117
saddamadashev <a.saddam77@icloud.com>
2026-02-28 09:39:41 +0500

    Create database.py

---
+++ database.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from config import DATABASE_URL
+
+engine = create_async_engine(DATABASE_URL, echo=False)
+SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+Base = declarative_base()
