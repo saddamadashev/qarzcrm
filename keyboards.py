a30ffa527e2afd0f2ee16e0986dcbe906191af67
saddamadashev <a.saddam77@icloud.com>
2026-02-28 18:57:14 +0500

    Delete keyboards.py

--- keyboards.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-def main_menu():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="➕ Mijoz qo‘shish", callback_data="add_customer")],
-        [InlineKeyboardButton(text="📋 Mijozlar", callback_data="list_customers")],
-        [InlineKeyboardButton(text="🏆 Reyting", callback_data="top")]
-    ])
